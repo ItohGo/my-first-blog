452eccb87c7416d80d06d62c5f2d46daec37b6eb
Go Itoh <matuzaki50@gmail.com>
2020-02-20 11:49:38 +0900

    記事一覧画面でのログイン不要の変更

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-@login_required
+#@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,7 +29,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-@login_required
+#@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -48,7 +48,7 @@ def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
-@login_required
+#@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
@@ -58,7 +58,7 @@ def publish(self):
     self.published_date = timezone.now()
     self.save()
 
-@login_required
+#@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
